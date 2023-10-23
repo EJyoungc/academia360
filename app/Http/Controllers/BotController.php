a3ea67407d5bf5c72ba863094e547fbb33820fc2
EJyoungc <nerst@live.com>
2023-10-23 20:02:23 +0200

    main

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -15,16 +15,18 @@ class BotController extends Controller
 {
     //
 
+    public $bot;
+    public $chat;
+    public $data;
+
 
     public function telegram(Request $request)
     {
         // $update = json_decode($request->getContent(), true);
         // event( new NewMessage($update));
-        $bot = TelegraphBot::find(1);
+        $this->bot = TelegraphBot::find(1);
+        $this->data = [
 
-        Log::channel('telegram')->debug('Incoming Telegram Webhook Data', [
-            // 'all'=>$request->all(),
-            // 'chat_test' => ,
             'update' => $request['update_id'] ?? "",
             'message_id' => $request['message']['message_id'] ?? "",
             'message_from_id' => $request['message']['from']['id'] ?? "",
@@ -36,26 +38,36 @@ class BotController extends Controller
             'message_body' => $request['message']['text'] ?? "",
             'message_type' => $request['entities'][0]['type'] ?? "",
             'query' => ''
+        ];
+        Log::channel('telegram')->debug('Incoming Telegram Webhook Data', [
+            'all'=>$request->all(),
+            // 'chat_test' => ,
+            // 'update' => $request['update_id'] ?? "",
+            // 'message_id' => $request['message']['message_id'] ?? "",
+            // 'message_from_id' => $request['message']['from']['id'] ?? "",
+            // 'bot' => $request['message']['from']['is_bot'] ?? "",
+            // 'message_firstname' => $request['message']['from']['first_name'] ?? "",
+            // 'message_lastname' => $request['message']['from']['last_name'] ?? "",
+            // 'message_type' => $request['message']['from']['type'] ?? "",
+            // 'message_chat_id' => $request['message']['chat']['id'] ?? "",
+            // 'message_body' => $request['message']['text'] ?? "",
+            // 'message_type' => $request['entities'][0]['type'] ?? "",
+            // 'query' => ''
         ]);
         $firstname = $request['message']['from']['first_name'] ?? "";
         $message_chat_id = $request['message']['chat']['id'] ?? "";
-        $message_id =$request['message']['message_id'] ?? "";
+        $message_id = $request['message']['message_id'] ?? "";
 
-        $chat2 = TelegraphChat::where("chat_id", $request['message']['chat']['id'] ?? "")->first();
-        if ($chat2 == null) {
-           $chat = $bot->chats()->create([
+        $this->chat = TelegraphChat::where("chat_id", $request['message']['chat']['id'] ?? "")->first();
+        if ($this->chat == null) {
+            $chat = $bot->chats()->create([
                 'chat_id' => $message_chat_id,
                 'name' => $firstname,
             ]);
             $chat->html("<strong>Hello!</strong>\n\nI'm here!")->send();
-
-           
         } else {
 
             // comand checker
-
-            
-
 
             switch ($request['message']['text']) {
                 case '/menu':
@@ -63,51 +75,21 @@ class BotController extends Controller
                     $chat2 = TelegraphChat::where("chat_id", $request['message']['chat']['id'] ?? "")->first();
                     // $chat2->html("<strong>Hello $firstname !</strong> \n\n how can i help you ?")->reply($message_id)->send();
                     $chat2->html("<strong>Hello $firstname !</strong> \n\n Please Select the Option Available")->keyboard(Keyboard::make()->buttons([
-                        Button::make("Class")->action("delete"),  
-                        Button::make("📖 Students")->action("read"),  
+                        Button::make("Class")->action("test"),
+                        Button::make("📖 Students")->action("read")->param('id','43'),
                         // Button::make("👀 ")->url('https://test.it'),  
                     ])->chunk(2))->send();
-                    
-                    
+
                     break;
-                
+                    // case''
+
                 default:
-                    
+
                     break;
             }
-
-
-
-
-
-
-
-
-
-
-
-           
         }
 
-        // $chat = $telegraph_bot->chats()->create([
-        //         'chat_id' => $firstname,
-        //         'name' => $message_chat_id,
-        //     ]);
-
-        // Your bot logic here
-
-        // return 'OK';
-        // $chat = $telegraph_bot->chats()->create([
-        //     'chat_id' => $firstname,
-        //     'name' => $message_chat_id,
-        // ]);
-        // $chat = TelegraphChat::find(1);
-        // // dd($chat);
-        // $chat->html("<strong>Hello!</strong>\n\nI'm here!")->send();
-        // Process the update (e.g., respond to user messages)
-        // Implement your bot's logic here
-
-        // Send a response (e.g., 200 OK)
+        
         return response('OK', 200);
     }
 
@@ -132,12 +114,14 @@ class BotController extends Controller
         }
     }
 
+    public function  test()
+    {
+        $this->chat->html("test")->send();
+    }
 
     private function showMenu1($update)
     {
         $text = "You are in Menu 1. What would you like to do next?";
-
-        
     }
 
     // Show Menu 2
@@ -150,9 +134,4 @@ class BotController extends Controller
             'text' => $text,
         ]);
     }
-
-
-
-
-
 }
