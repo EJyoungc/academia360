6683ce90074102768beecda93090e70feb206f56
EJyoungc <nerst@live.com>
2023-10-25 10:21:37 +0200

    main

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -68,7 +68,8 @@ class BotController extends Controller
                     ];
                     $response = Telegram::sendMessage([
                         'chat_id' => $chat_id,
-                        'text' => "Hello <strong> $full_name</strong> <br><br> <strong> All Academic Class :</strong>  ",
+                        'text' => "Hello  **$full_name** \n\n  **All Academic Class :**  ",
+                        'parse_mode' => 'Markdown',
                         'reply_markup' => json_encode($keyboard),
                     ]);
                     break;
