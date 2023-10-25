71126388aa58ff1980405ddc665f35dbfb0f7535
EJyoungc <nerst@live.com>
2023-10-25 21:07:00 +0200

    main

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -83,8 +83,8 @@ class BotController extends Controller
 
         if (isset($request['message']['chat']['id'])) {
             Log::channel('telegram')->debug('is working', [
-                'text' =>$request['message']['text']??'',
-                
+                'text' =>$request['message']['text']??'/years',
+
                 'data' =>$request['message']['chat']['id']??'',
                 'name' =>$request['message']['chat']['first_name']??''
                 //  'chat_id' =>$request['callback_query']['message']['chat']['id']??'',
