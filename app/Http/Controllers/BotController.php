c1b66dbb2ceb42b059bfbc69be609709bae7131e
EJyoungc <nerst@live.com>
2023-10-25 19:43:25 +0200

    main

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -66,12 +66,12 @@ class BotController extends Controller
 
         if (isset($request['callback_query']['data'])) {
 
-            // Log::channel('telegram')->debug('is reply', [
+            Log::channel('telegram')->debug('is reply', [
             //     'data' =>$request['callback_query']['data']??'',
             //      'chat_id' =>$request['callback_query']['message']['chat']['id']??'',
             //     'all' => $request->all(),
 
-            // ]);
+            ]);
             $this->selectoption(
                 $request['callback_query']['message']['chat']['id'],
                 $request['callback_query']['message']['chat']['first_name'] ,
@@ -80,14 +80,14 @@ class BotController extends Controller
 
 
         if (isset($request['message']['chat']['id'])) {
-            // Log::channel('telegram')->debug('is working', [
-            //     'data' =>$request['callback_query']['data']??'',
-            //      'chat_id' =>$request['callback_query']['message']['chat']['id']??'',
-            //     'all' => $request->all(),
-            //      'test' => $request['chat_instance'],
+            Log::channel('telegram')->debug('is working', [
+                // 'data' =>$request['callback_query']['data']??'',
+                //  'chat_id' =>$request['callback_query']['message']['chat']['id']??'',
+                // 'all' => $request->all(),
+                //  'test' => $request['chat_instance'],
 
-            //     'test'=>$response, 
-            // ]);
+                // 'test'=>$response, 
+            ]);
             $this->menu($message, $chat_id, $full_name);
         }
 
