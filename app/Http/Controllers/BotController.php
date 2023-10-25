793ecd41b8f65a237d8fe3e6aec2747bcb58bc48
EJyoungc <nerst@live.com>
2023-10-25 11:59:38 +0200

    main

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Events\NewMessage;
+use App\Models\Classroom;
 use App\Models\ClassRoomType;
 // use DefStudio\Telegraph\Keyboard\Button;
 // use DefStudio\Telegraph\Keyboard\Keyboard;
 // use DefStudio\Telegraph\Models\TelegraphBot;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 // use DefStudio\Telegraph\Models\TelegraphChat;
 // use DefStudio\Telegraph\Telegraph;
 use Illuminate\Support\Facades\Log;
@@ -37,8 +39,8 @@ class BotController extends Controller
 
     public function telegram(Request $request)
     {
-        
-        
+
+
         Log::channel('telegram')->debug('Data feed', [
             'all' => $request->all(),
             // 'test' => Telegram::getWebhookUpdate(),
@@ -46,15 +48,13 @@ class BotController extends Controller
         ]);
 
         if (isset($request['message']['chat']['id'])) {
-            
+
             $message = $request['message']['text'];
             $chat_id = $request['message']['chat']['id'];
-            $full_name = $request['message']['from']['first_name'] .' '. $request['message']['from']['last_name'];
-            $this->menu($message,$chat_id,$full_name);
-
-
-
-
+            $full_name = $request['message']['from']['first_name'] . ' ' . $request['message']['from']['last_name'];
+            // $this->select_option($request);
+            $this->selectoption($request);
+            $this->menu($message, $chat_id, $full_name);
         }
 
 
@@ -86,21 +86,76 @@ class BotController extends Controller
     }
 
 
-    public function menu($message,$chat_id,$full_name){
+
+    public function selectoption($data)
+    {
+
+        if (isset($data['callback_query'])) {
+            $callbackData = $data['callback_query']['data'];
+
+            
+            list($model, $id) = explode(' ', $callbackData);
+
+            $chatId = $data['callback_query']['message']['chat']['id'];
+
+            // Handle different options based on the custom data
+            switch ($model) {
+                case 'classrooms':
+                    $classroomtype = ClassRoomType::find($id);
+                    $classrooms = Classroom::where('classroom_id',$id)->orderBy('name', 'asc')->get();
+                    $buttons = [];
+
+                    foreach ($classrooms as  $class) {
+                        array_push($buttons, ['text' => " 🎓 $class->name", "callback_data" => "students  $class->id"]);
+                    }
+                    $buttonsInRow = 2;
+                    $keyboard = [
+
+                        'inline_keyboard' => array_chunk($buttons, $buttonsInRow),
+                    ];
+                    $response = Telegram::sendMessage([
+                        'chat_id' => $chat_id,
+                        'text' => " Hey 😊 👋' *$full_name* *Here are the Class Rooms in $classroomtype->name  you wanted*👇  ",
+                        'parse_mode' => 'Markdown',
+                        'reply_markup' => json_encode($keyboard),
+                    ]);
+
+                    break;
+                case 'option_2_data':
+                    $text = 'You selected Option 2.';
+                    break;
+                case 'option_3_data':
+                    $text = 'You selected Option 3.';
+                    break;
+                default:
+                    $text = 'Invalid selection.';
+                    break;
+            }
+        }
+    }
+
+
+
+
+
+
+
+    public function menu($message, $chat_id, $full_name)
+    {
 
         switch ($message) {
             case '/years':
                 # code...
-                $classes = ClassRoomType::orderBy('name','asc')->get();
-                $buttons= [];
+                $classes = ClassRoomType::orderBy('name', 'asc')->get();
+                $buttons = [];
 
-                foreach($classes as  $class){
-                    array_push($buttons,['text'=>" 🎓 $class->name", "callback_data"=>"$class->id"]);
+                foreach ($classes as  $class) {
+                    array_push($buttons, ['text' => " 🎓 $class->name", "callback_data" => "classrooms $class->id"]);
                 }
                 $buttonsInRow = 2;
                 $keyboard = [
-                    
-                    'inline_keyboard' => array_chunk($buttons,$buttonsInRow),
+
+                    'inline_keyboard' => array_chunk($buttons, $buttonsInRow),
                 ];
                 $response = Telegram::sendMessage([
                     'chat_id' => $chat_id,
@@ -111,15 +166,10 @@ class BotController extends Controller
                     'reply_markup' => json_encode($keyboard),
                 ]);
                 break;
-            
+
             default:
                 # code...
                 break;
         }
-
-
     }
-
-
-
 }
