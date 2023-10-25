aaa716ec17014aa8f957d3acd96f5b8097a515b6
EJyoungc <nerst@live.com>
2023-10-25 16:58:08 +0200

    main

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -66,7 +66,7 @@ class BotController extends Controller
             ]);
             $this->selectoption(
                 $request['callback_query']['message']['chat']['id'],
-                $request['callback_query']['message']['from']['first_name'] . ' ' . $request['message']['from']['last_name'],
+                $request['callback_query']['message']['from']['first_name'] . ' ' . $request['callback_query']['message']['from']['last_name'],
                 $request['callback_query']['data']);
         }
 
