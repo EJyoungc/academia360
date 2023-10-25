5657e40d690d528dbf42d35ddd676f14bbc8fe22
EJyoungc <nerst@live.com>
2023-10-25 13:29:30 +0200

    main

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -43,7 +43,7 @@ class BotController extends Controller
 
         Log::channel('telegram')->debug('Data feed', [
             'data' =>$request['callback_query']['data']??'',
-             'data' =>$request['message']['chat']['id']??'',
+             'chat_id' =>$request['message']['chat']['id']??'',
             'all' => $request->all(),
             //  'test' => $request['chat_instance'],
              
